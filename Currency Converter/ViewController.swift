a479cd3fcd6c78aa7a3eb195ae69fabcfe185e95
simon wilson <simonhanwilson@hotmail.co.uk>
2020-12-20 23:41:50 +0000

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Simon Wilson on 20/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
